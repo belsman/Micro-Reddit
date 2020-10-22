c26260cb623d7eca95149024bb5aca1cdaf216bb
Bello S Babakolo <fleetbeekay@gmail.com>
2020-10-22 12:32:29 +0100

    Associate Post model to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
     validates :username, :email, :password, presence: true
-
     validates :username, length: { minimum: 3 }
-    
     validates :password, length: { in: 6..20 }
-
     validates_uniqueness_of :username, :email
+
+    has_many :posts, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_203806) do
+ActiveRecord::Schema.define(version: 2020_10_22_111142) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "link"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2020_10_21_203806) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
