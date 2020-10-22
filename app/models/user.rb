2522b9d3bbea4418208688f7b4dfacf6393f9ee6
Bello S Babakolo <fleetbeekay@gmail.com>
2020-10-22 11:51:29 +0100

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :username, :email, :password, presence: true
+
+    validates :username, length: { minimum: 3 }
+    
+    validates :password, length: { in: 6..20 }
+
+    validates_uniqueness_of :username, :email
 end
